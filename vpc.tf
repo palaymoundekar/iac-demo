43c5f02e89f3210675dac7ff03208baf531b386c
moundekarpalay <101330098+moundekarpalay@users.noreply.github.com>
2022-04-28 21:20:55 +0530

    uodate3

--- vpc.tf
+++ vpc.tf
@@ -84,7 +84,7 @@ resource "aws_security_group" "allow_http" {
 resource "aws_launch_configuration" "web" {
   name_prefix = "web-"
 
-  image_id = "ami-04893cdb768d0f9ee"
+  image_id = "ami-0f9fc25dd2506cf6d"
   instance_type = "t2.micro"
 
   security_groups = [ aws_security_group.allow_http.id ]
