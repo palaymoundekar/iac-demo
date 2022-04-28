ef8fc441871ad8ddadd462b7133e99f8532e940d
moundekarpalay <101330098+moundekarpalay@users.noreply.github.com>
2022-04-28 21:35:42 +0530

    update12

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.0.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "Private Subnet"
@@ -20,7 +20,7 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-south-1b"
+  availability_zone = "us-east-2b"
 
   tags = {
     Name = "Public Subnet"
@@ -81,113 +81,3 @@ resource "aws_security_group" "allow_http" {
 
 }
 
-resource "aws_launch_configuration" "web" {
-  name_prefix = "web-"
-
-  image_id = "ami-0f9fc25dd2506cf6d"
-  instance_type = "t2.micro"
-
-  security_groups = [ aws_security_group.allow_http.id ]
-  associate_public_ip_address = true
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_security_group" "elb_http" {
-  name        = "elb_http"
-  description = "Allow HTTP traffic to instances through Elastic Load Balancer"
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Allow HTTP through ELB Security Group"
-  }
-}
-
-resource "aws_elb" "web_elb" {
-  name = "web-elb"
-  security_groups = [
-    aws_security_group.elb_http.id
-  ]
-  subnets = [
-    aws_subnet.private_subnet.id,
-    aws_subnet.public_subnet.id
-  ]
-
-  cross_zone_load_balancing   = true
-
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    interval = 30
-    target = "HTTP:80/"
-  }
-
-  listener {
-    lb_port = 80
-    lb_protocol = "http"
-    instance_port = "80"
-    instance_protocol = "http"
-  }
-
-}
-
-
-
-resource "aws_autoscaling_group" "web" {
-  name = "asg"
-
-  min_size             = 1
-  desired_capacity     = 2
-  max_size             = 4
-  
-  health_check_type    = "ELB"
-  load_balancers = [
-    aws_elb.web_elb.id
-  ]
-
-  launch_configuration = aws_launch_configuration.web.name
-
-  enabled_metrics = [
-    "GroupMinSize",
-    "GroupMaxSize",
-    "GroupDesiredCapacity",
-    "GroupInServiceInstances",
-    "GroupTotalInstances"
-  ]
-
-  metrics_granularity = "1Minute"
-
-  vpc_zone_identifier  = [
-    aws_subnet.private_subnet.id,
-    aws_subnet.public_subnet.id
-  ]
-
-  # Required to redeploy without an outage.
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "web"
-    propagate_at_launch = true
-  }
-
-}
